79afb3bd101f1439427ef34096f38c6615cbd4ce
iyasmzn <58409100+iyasmzn@users.noreply.github.com>
2024-01-06 16:24:58 +0700

    dynamic fieldname in purchase_cost input

--- resources/views/partials/forms/edit/purchase_cost.blade.php
+++ resources/views/partials/forms/edit/purchase_cost.blade.php
@@ -1,9 +1,9 @@
 <!-- Purchase Cost -->
-<div class="form-group {{ $errors->has('purchase_cost') ? ' has-error' : '' }}">
-    <label for="purchase_cost" class="col-md-3 control-label">{{ trans('general.purchase_cost') }}</label>
+<div class="form-group {{ $errors->has(($fieldname ?? 'purchase_cost')) ? ' has-error' : '' }}">
+    <label for="{{ $fieldname ?? 'purchase_cost' }}" class="col-md-3 control-label">{{ trans('general.purchase_cost') }}</label>
     <div class="col-md-9">
         <div class="input-group col-md-4" style="padding-left: 0px;">
-            <input class="form-control" type="text" name="purchase_cost" aria-label="purchase_cost" id="purchase_cost" value="{{ old('purchase_cost', Helper::formatCurrencyOutput($item->purchase_cost)) }}" />
+            <input class="form-control" type="text" name="{{ $fieldname ?? 'purchase_cost' }}" aria-label="{{ $fieldname ?? 'purchase_cost' }}" id="{{ $fieldname ?? 'purchase_cost' }}" value="{{ old(($fieldname ?? 'purchase_cost'), Helper::formatCurrencyOutput($item->{$fieldname ?? 'purchase_cost'})) }}" />
             <span class="input-group-addon">
                 @if (isset($currency_type))
                     {{ $currency_type }}
@@ -13,7 +13,7 @@
             </span>
         </div>
         <div class="col-md-9" style="padding-left: 0px;">
-            {!! $errors->first('purchase_cost', '<span class="alert-msg" aria-hidden="true"><i class="fas fa-times" aria-hidden="true"></i> :message</span>') !!}
+            {!! $errors->first(($fieldname ?? 'purchase_cost'), '<span class="alert-msg" aria-hidden="true"><i class="fas fa-times" aria-hidden="true"></i> :message</span>') !!}
         </div>
     </div>
 
